8cabe839d953054fd3bc74e720f1433440fc3792
Albin Romlin <albin@romlin.se>
2020-11-13 01:53:07 +0100

    first implementation 90% fin

---
+++ learnrussian.py
@@ -0,0 +1,114 @@
+import random
+
+i = 0
+lista = []
+one = ""
+two = ""
+antalHint = 5
+hintList = []
+randList = []
+
+
+dic = {
+    'А': 'a',
+    'Б': 'b',
+    'В': 'v',
+    'Г': 'g',
+    'Д': 'd',
+    'E': 'ye',
+    'Ё': 'yo',
+    'Ж': 's*',
+    'З': 'z',
+    'И': 'ee',
+    'Й': 'y',
+    'К': 'k',
+    'Л': 'l',
+    'М': 'm',
+    'Н': 'n',
+    'О': 'o',
+    'П': 'p',
+    'Р': 'r',
+    'С': 's',
+    'Т': 't',
+    'Ф': 'f',
+    'Х': 'ch*',
+    'Ц': 'ts',
+    'Ч': 'ch',
+    'Щ': 'sh',
+    'Ы': 'u',
+    'Э': 'e',
+    'Ю': 'ou',
+    'Я': 'ya'}
+
+for key in dic:
+    lista.append(key)
+
+def fillHintList:
+    for x in range (antalHint):
+    hintList.append("")
+
+def findRandomRus:
+    temp = random.randint(0,27)
+    facit = dic[lista[temp]]
+    print(lista[temp])
+
+
+while True:
+    print("answer in what language")
+    one = input("rus(r) / eng(e): ").lower()
+    if one == "r" or one == "e":
+        break
+
+
+if one == "e":
+    print("Skriv engelska bokstaven/lätet")
+    print("'exit' / 'help' / 'hint'")
+    print("====================")
+    randList = []
+    hintList = []
+    fillHintList()
+    while True:
+        findRandomRus()
+        while True:
+            two = input("svar: ").lower()
+            if two == "exit":
+                break
+            elif two == "help":
+                print(facit + "\n===================")
+                break
+            elif two == facit:
+                print("RÄTT\n===================")
+                break
+            elif two == "hint":
+                temp2 = random.randint(0,4)
+                hintList[temp2] = facit
+                randList.append(temp2)
+                
+                for x in range (antalHint):
+                    while True:
+                        temp2 = random.randint(0, 4)
+                        if ((temp2 in randList) == False):
+                            break
+                    hintList[random.randint(0, 4)] = dic[lista[random.randint(0,27)]]
+                    randList.append(temp2)
+                print(hintList)
+                
+        if two == "exit":
+            break
+                
+
+
+##NOT FINNISHED
+elif one == "r":
+    print("======================================")
+    print(lista)
+    
+
+##if "model" in thisdict:
+##  print("Yes, 'model' is one of the keys in the thisdict dictionary")
+    
+##print("А, Б, В, Г, Д, Е, Ё, Ж, З, И, Й, К, Л, М, Н, О, П, Р, С, Т, У, Ф, Х, Ц, Ч, Ш, Щ, Ъ, Ы, Ь, Э, Ю, Я")
+    
+
+print("out")
+
